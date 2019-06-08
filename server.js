f60ad0209b3fe5da81f756d0f59fac717a2995c5
Jake Murzy <jake@murzy>
2019-06-08 13:47:59 -0700

    koaify server

--- server.js
+++ server.js
@@ -1,15 +1,63 @@
-const { createServer } = require('http')
-const next = require('next')
-const routes = require('./routes')
+const os = require('os');
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const koaHelmet = require('koa-helmet');
+const statuses = require('statuses');
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handler = routes.getRequestHandler(app)
+const createNextApp = require('next');
 
-app.prepare().then(() => {
-  createServer(handler).listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+const nextRoutes = require('./routes');
+
+const PORT = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+
+const nextApp = createNextApp({ dev });
+const nextHandle = nextRoutes.getRequestHandler(nextApp);
+
+const koaRouter = new KoaRouter();
+
+koaRouter.get('*', async (ctx) => {
+  await nextHandle(ctx.req, ctx.res);
+
+  // Bypass Koa's built-in response handling. This isn't supported by Koa. So using with caution
+  ctx.respond = false;
+});
+
+const run = async () => {
+  await nextApp.prepare();
+
+  const koaApp = new Koa();
+
+  // Koa doesn't seems to set the default statusCode. So, do that first
+  koaApp.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+
+  if (!dev) {
+    koaApp.use(koaHelmet.hsts());
+    koaApp.use(async (ctx, next) => {
+      const proto = ctx.req.headers['x-forwarded-proto'];
+
+      // Enforce SSL & HSTS in production
+      if (proto !== 'https') {
+        const to = `https://${ctx.req.headers.host}${ctx.req.url}`;
+
+        ctx.res.writeHead(statuses('Found'), { Location: to });
+        ctx.res.end();
+        return;
+      }
+
+      await next();
+    });
+  }
+
+  koaApp.use(koaRouter.routes());
+
+  koaApp.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`> Ready on http://localhost:${PORT} with ${os.cpus().length} CPUs.`);
+  });
+};
+
+run();
