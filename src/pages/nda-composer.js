6e13ffbb232f740954d5f7646a971fc664473b28
Julia Qiu <juliaqiuxy@gmail.com>
2020-04-23 20:17:54 -0700

    forbid sending emails to self

--- src/pages/nda-composer.js
+++ src/pages/nda-composer.js
@@ -5,16 +5,33 @@ import { API } from '../api';
 import { Router } from '../routes';
 import * as sessionStorage from '../lib/sessionStorage';
 
+import { toQueryString } from '../util';
+
 const NDAComposerPage = (props) => {
   const nda = useMemo(() => sessionStorage.getItem('nda'), []);
+
+  const senderEmail = props.user.metadata.linkedInProfile.emailAddress;
+  const recipientIsSelf = nda?.recipientEmail === senderEmail;
 
   useEffect(() => {
     if (!nda) {
       Router.replaceRoute('/');
+      return;
     }
-  }, [nda]);
 
-  if (!nda) {
+    if (recipientIsSelf) {
+      const qs = toQueryString({
+        errorMessage: 'You can not send an NDA to yourself',
+      });
+
+      Router.replaceRoute(`/nda/new?${qs}`);
+      // let me return for god's sake
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
+  }, [nda, recipientIsSelf]);
+
+  if (!nda || recipientIsSelf) {
     return null;
   }
 
