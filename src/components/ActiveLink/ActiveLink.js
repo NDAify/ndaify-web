aa418bc32493abaa3a16f411fa554ec631137008
Jake Murzy <jake@murzy>
2020-04-30 19:50:17 -0700

    fix ActiveLink to fallback on href

--- src/components/ActiveLink/ActiveLink.js
+++ src/components/ActiveLink/ActiveLink.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { withRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-const ActiveLink = withRouter(({ router, children, ...props }) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Link {...props}>{children((props.as) === router.asPath)}</Link>
-));
+const ActiveLink = ({ children, ...props }) => {
+  const router = useRouter();
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Link {...props}>{children((props.as || props.href) === router.asPath)}</Link>
+  );
+};
 
 export default ActiveLink;
