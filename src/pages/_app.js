d5c434c89154b74c179e5a59304e97134699b24e
Jake Murzy <jake@murzy>
2020-05-03 14:28:11 -0700

    fix scroll on page transition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -53,6 +53,7 @@ Router.events.on('routeChangeStart', () => {
   NProgress.start();
 });
 Router.events.on('routeChangeComplete', () => {
+  window.scrollTo(0, 0);
   NProgress.done();
 });
 Router.events.on('routeChangeError', () => NProgress.done());
