f9ade036824d5b7247a3db50f3eac40f6cb3ad5f
Jake Murzy <jake@murzy>
2020-05-28 23:25:04 -0700

    focus document on route change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -171,6 +171,7 @@ Router.events.on('routeChangeStart', () => {
 });
 Router.events.on('routeChangeComplete', () => {
   window.scrollTo(0, 0);
+  document.body.focus();
   NProgress.done();
 });
 Router.events.on('routeChangeError', () => NProgress.done());
