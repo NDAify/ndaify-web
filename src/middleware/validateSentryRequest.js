1fd04ac3ef1ca88ce24f5cc90a25cb8794e59753
Jake Murzy <jake@murzy>
2020-06-05 15:00:45 -0700

    guard sourcemaps only in prod

--- src/middleware/validateSentryRequest.js
+++ src/middleware/validateSentryRequest.js
@@ -4,36 +4,39 @@ import loggerClient from '../db/loggerClient';
 
 const {
   SENTRY_SECRET_TOKEN,
+  NODE_ENV,
 } = process.env;
 
 export default async (ctx, next) => {
-  const {
-    'x-sentry-token': sentryToken,
-  } = ctx.request.headers;
+  if (NODE_ENV !== 'development') {
+    const {
+      'x-sentry-token': sentryToken,
+    } = ctx.request.headers;
 
-  let verified;
-  try {
-    if (!sentryToken) {
-      throw new Error('Missing Sentry token');
+    let verified;
+    try {
+      if (!sentryToken) {
+        throw new Error('Missing Sentry token');
+      }
+
+      if (sentryToken !== SENTRY_SECRET_TOKEN) {
+        throw new Error('Invalid Sentry token');
+      }
+
+      verified = true;
+    } catch (error) {
+      // eslint-disable-next-line
+      loggerClient.info(error);
     }
 
-    if (sentryToken !== SENTRY_SECRET_TOKEN) {
-      throw new Error('Invalid Sentry token');
+    if (!verified) {
+      ctx.status = statuses('Unauthorized');
+      ctx.body = {
+        errorMessage: 'Request originates from a non-Sentry source',
+      };
+
+      return;
     }
-
-    verified = true;
-  } catch (error) {
-    // eslint-disable-next-line
-    loggerClient.info(error);
-  }
-
-  if (!verified) {
-    ctx.status = statuses('Unauthorized');
-    ctx.body = {
-      errorMessage: 'Request originates from a non-Sentry source',
-    };
-
-    return;
   }
 
   await next();
