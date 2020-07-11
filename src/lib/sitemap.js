19c758134bf897788e4cd54854d90361ea85bb38
Julia Qiu <julia@julia.dev>
2020-07-11 11:37:06 -0700

    generate sitemap dynamically

--- src/lib/sitemap.js
+++ src/lib/sitemap.js
@@ -1,25 +1,55 @@
-import { createSitemap } from 'sitemap';
-import { promisify } from 'util';
+import fetch from 'cross-fetch';
 import statuses from 'statuses';
-
-const sm = createSitemap({
-  hostname: 'https://ndaify.com',
-});
-
-const toXML = promisify(sm.toXML).bind(sm);
-
-// See https://github.com/ekalinin/sitemap.js
-sm.add({
-  url: '/',
-  changefreq: 'weekly',
-  priority: 1,
-});
+import { SitemapStream, streamToPromise } from 'sitemap';
 
 const sitemap = () => async (ctx) => {
   try {
-    const SITEMAP_XML = await toXML();
+    const sm = new SitemapStream({
+      hostname: process.env.CANONICAL_URL,
+      lastmodDateOnly: false,
+      xmlns: {
+        news: true,
+        xhtml: true,
+        image: true,
+        video: true,
+      },
+    });
+
+    sm.write({
+      url: '/',
+      changefreq: 'weekly',
+      priority: 1,
+      img: [
+        {
+          url: 'https://ndaify.com/images/meta.png',
+          caption: 'NDAify helps you keep your secrets under wraps.',
+          title: 'NDAify',
+        },
+      ],
+    });
+
+    // const ndaifyService = new NdaifyService();
+    // const ndaTemplateOptions = await ndaifyService.getNdaTemplateOptions();
+    const response = await fetch(`${process.env.NDAIFY_ENDPOINT_URL}/nda-templates/options`, {
+      method: 'GET',
+    });
+
+    const { ndaTemplateOptions } = await response.json();
+
+    ndaTemplateOptions.forEach((opt) => {
+      sm.write({
+        url: `/sample/${opt.ndaTemplateId}`,
+        changefreq: 'weekly',
+        priority: 1,
+      });
+    });
+
+    sm.end();
+
+    const xml = await streamToPromise(sm);
+
     ctx.set('Content-Type', 'text/xml');
-    ctx.res.write(SITEMAP_XML);
+    ctx.res.write(xml.toString());
   } catch {
     ctx.status = statuses('Internal Server Error');
   } finally {
